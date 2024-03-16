4301a956a136eb4fd3a67bba57d8c8e7fc3beb27
yaochenliu <yaochenliu@microsoft.com>
2024-03-16 13:59:27 +0800

    remove unused files

--- after_deploy.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "yaochenliu@microsoft.com"
-git config --global user.name "yaochenliu"
--- before_deploy.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "liujussen@gmail.com"
-git config --global user.name "eurekaffeine"
