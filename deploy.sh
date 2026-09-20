91afec68bec48c8afad8a5430406a3b7785f83f6
eurekaffeine <32728109+eurekaffeine@users.noreply.github.com>
2026-09-20 23:20:27 +0800

    fix: update GitHub Pages deployment

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,13 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+touch .nojekyll
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:eurekaffeine/pocket-gallery.git main:gh-pages
+git push -f https://github.com/eurekaffeine/pocket-gallery.git HEAD:gh-pages
 
 cd -
